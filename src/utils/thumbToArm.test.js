88ac43fea2b918ee8164f1724267d24200d163d8
vinicius-CL <vinicius_lopes15@usp.br>
2023-07-23 00:17:32 -0300

    Formatos 11, 14 e 15\n\n\tPequenas correções

--- src/utils/thumbToArm.test.js
+++ src/utils/thumbToArm.test.js
@@ -26,4 +26,24 @@ test("teste formato 9 - store with immediate offset", () => {
     expect(store).toBe(0b1110_01_0_1_1_0_0_0_0010_0011_000000000110);
 });
 
+test("teste formato 11 - load SP-relative immediate offset", () => {
+    const ldr_sp = thumbToArm(0b1001_1_011_00000000);
+    expect(ldr_sp).toBe(0b1110_01_0_1_1_0_0_1_1101_0011_000000000000);
+});
+
+test("teste formato 14 - push {Rlist, LR}", () => {
+    const push = thumbToArm(0b1011_0_10_1_11011111); // push R13! ,{Rlist, LR}
+    expect(push).toBe(0b1110_100_1_0_0_1_0_1101_0100000011011111); // stmdb r13!, {Rlist, R14}
+});
+
+test("teste formato 14 - pop {Rlist, PC}", () => {
+    const pop = thumbToArm(0b1011_1_10_1_11011111); // pop R13! ,{Rlist, PC}
+    expect(pop).toBe(0b1110_100_0_1_0_1_1_1101_1000000011011111); // ldmia r13!, {Rlist, R15}
+});
+
+test("teste formato 15 - multiple load with reg list", () => {
+    const mult_load = thumbToArm(0b1100_1_011_11010011); // ldmia Rb!, {Rlist}
+    expect(mult_load).toBe(0b1110_100_0_1_0_1_1_0011_0000000011010011); // ldmia r3!, {Rlist}
+});
+
 
