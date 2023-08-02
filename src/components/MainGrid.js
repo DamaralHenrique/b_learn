0bb8b8c59bafd89e894cf2e7f80a25971cc28d36
DamaralHenrique <rique01101999@gmail.com>
2023-08-01 21:01:28 -0300

    Add main grid component

---
+++ src/components/MainGrid.js
@@ -0,0 +1,59 @@
+import { COLORS } from '../utils/Colors.js';
+import BitBox from './BitBox.js';
+
+function MainGrid({bits, instruction, isThumb, change_bits, message }) {
+
+  const Styles = {
+    background: {
+      fontFamily: 'Arial',
+      height: window.innerHeight/2,
+    },
+    bitRowDiv: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    center: {
+      height: "100%",
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'column'
+    },
+    instructionText: {
+      paddingTop: 10,
+      fontSize: 40,
+      color: isThumb ? COLORS.black : COLORS.white,
+    },
+    messageText: {
+      paddingTop: 20,
+      fontSize: 20,
+      color: isThumb ? COLORS.black : COLORS.red,
+    },
+    title: {
+      fontSize: 100,
+      fontFamily: 'Arial',
+      fontWeight: 'bold',
+      textAlign: isThumb ? 'left' : 'right',
+      color: isThumb ? COLORS.black : COLORS.green,
+      top: isThumb ? -90 : window.innerHeight/2 - 22,
+      left: !isThumb ? window.innerWidth - 230 - 30 : 30,
+      position: isThumb ? 'relative' : 'absolute'
+    }
+  };
+
+  return (
+    <div style={{backgroundColor: isThumb ? COLORS.green : COLORS.black, ...Styles.background}}>
+      {!isThumb && <div style={Styles.title}>ARM</div>}
+      <div style={Styles.center}>
+        <div style={Styles.bitRowDiv}>
+          {bits.map((element, index) => <BitBox value={element} onPress={() => change_bits && change_bits(index)}/>)}
+        </div>
+        {message && <a style={Styles.messageText}>{message}</a>}
+        <a style={Styles.instructionText}>{instruction}</a>
+      </div>
+      {!!isThumb && <div style={Styles.title}>THUMB</div>}
+    </div>
+  );
+}
+  
+export default MainGrid;
